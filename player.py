14d6a18f91fccca33ed33cc6099d85aa25057603
Csprier <Csprier@gmail.com>
2022-08-23 18:06:40 -0400

    The green box moves when you push the arrow keys. :)

--- player.py
+++ player.py
@@ -12,8 +12,9 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=pos)
 
         # Movement attributes
-        self.direction = pygame.Vector2()  # default value is 0,0 or nothing
-
+        self.direction = pygame.math.Vector2() # default value is 0,0 or nothing
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
     def input(self):
         # .get_pressed() returns a list of all the keys that are potentially being pressed
         keys = pygame.key.get_pressed()
@@ -25,7 +26,7 @@ class Player(pygame.sprite.Sprite):
             print('Down')
             self.direction.y = 1
         else:
-            self.direction = 0
+            self.direction.y = 0
 
         if keys[pygame.K_LEFT]:
             print('Left')
@@ -34,6 +35,11 @@ class Player(pygame.sprite.Sprite):
             print('Right')
             self.direction.x = 1
         else:
-            self.direction = 0
+            self.direction.x = 0
+
+    def move(self, dt):
+        self.pos += self.direction * self.speed * dt
+        self.rect.center = self.pos
     def update(self, dt):
         self.input()
+        self.move(dt)

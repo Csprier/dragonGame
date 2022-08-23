5987d8f1a4c768cf2c3a7edfd78d87ac411788bc
Csprier <Csprier@gmail.com>
2022-08-23 16:50:47 -0400

    Updated line 9 of main.py with a title for the game window.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('Dragon Game')
         self.clock = pygame.time.Clock()
 
     # Run method which is responsible for the 'game loop'

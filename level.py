0762d20139f2cdb75a7b1fba88a4f646d30d8843
Csprier <Csprier@gmail.com>
2022-08-23 17:58:35 -0400

    Implemented movement conditionals in player.py

--- level.py
+++ level.py
@@ -5,6 +5,7 @@ from player import Player
 
 class Level:
     def __init__(self):
+        # Setup
         # Get the display surface
         self.display_surface = pygame.display.get_surface()
         # Sprite groups
@@ -15,7 +16,7 @@ class Level:
         self.player = Player((640, 360), self.all_sprites)
 
     def run(self, dt):
-        print('This is the level.')
+        # print('This is the level.')
         self.display_surface.fill('black')
         self.all_sprites.draw(self.display_surface)
-        self.all_sprites.update()
+        self.all_sprites.update(dt)
--- player.py
+++ player.py
@@ -5,6 +5,35 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
+
+        # General setup
         self.image = pygame.Surface((32, 64))
         self.image.fill('green')
         self.rect = self.image.get_rect(center=pos)
+
+        # Movement attributes
+        self.direction = pygame.Vector2()  # default value is 0,0 or nothing
+
+    def input(self):
+        # .get_pressed() returns a list of all the keys that are potentially being pressed
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            print('Up')
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            print('Down')
+            self.direction.y = 1
+        else:
+            self.direction = 0
+
+        if keys[pygame.K_LEFT]:
+            print('Left')
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT]:
+            print('Right')
+            self.direction.x = 1
+        else:
+            self.direction = 0
+    def update(self, dt):
+        self.input()

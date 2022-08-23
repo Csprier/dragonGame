569e30fe18efc841ed46c2c66b601cfef775f7c5
Csprier <Csprier@gmail.com>
2022-08-23 17:12:32 -0400

    Updated settings.py to have game settings. Created and imported the Level class from level.py into main.py and set it to run the level.

---
+++ level.py
@@ -0,0 +1,10 @@
+import pygame
+from settings import *
+
+
+class Level:
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+
+    def run(self, dt):
+        print('This is the level.')
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame, sys
 from settings import *
-
+from level import Level
 
 class Game:
     def __init__(self):
@@ -8,6 +8,7 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption('Dragon Game')
         self.clock = pygame.time.Clock()
+        self.level = Level()
 
     # Run method which is responsible for the 'game loop'
     # While the game is open, update. Otherwise, the game quits and shuts down entirely.
@@ -18,7 +19,10 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
+            # Delta Time
+            # dt is passed to the run method of the level class to keep the frames updating
             dt = self.clock.tick() / 1000
+            self.level.run(dt)
             pygame.display.update()
 
 
